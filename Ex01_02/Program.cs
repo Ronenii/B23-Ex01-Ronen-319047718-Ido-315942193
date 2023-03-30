0036b0e778f5f9b52e395d9db89375dc37277096
idobi <idobi@mta.ac.il>
2023-03-30 15:19:50 +0300

    implement ex3

--- Ex01_02/Program.cs
+++ Ex01_02/Program.cs
@@ -2,12 +2,12 @@
 
 namespace Ex01_02
 {
-    class Program
+    public class Program
     {
         public static void Main()
         {
-            int ROWS_AMOUNT = 5;
-            PrintDiamond(ROWS_AMOUNT);
+            const int k_RowsAmount = 5;
+            PrintDiamond(k_RowsAmount);
             Console.Read();
         }
 
@@ -18,16 +18,13 @@ namespace Ex01_02
 
         private static void printDiamondWrapper(int i_NumberOfRows, int i_WorkingLine)
         {
-            int i, numberOfSpaces;
-            int starsAmount = -1;
-
             if (i_NumberOfRows <= 0)
             {
                 return;
             }
 
-            numberOfSpaces = i_NumberOfRows - 1;
-            starsAmount = calculateAmountOfStars(i_WorkingLine);
+            int numberOfSpaces = i_NumberOfRows - 1;
+            int starsAmount = calculateAmountOfStars(i_WorkingLine);
             printShspeLine(numberOfSpaces, starsAmount);
             printDiamondWrapper(--i_NumberOfRows, i_WorkingLine + 1);
             numberOfSpaces = i_NumberOfRows + 1;
---
+++ Ex01_03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex01_03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            RunExpertDiamond();
+            Console.Read();
+        }
+        public static void RunExpertDiamond()
+        {
+            bool inputValidation = false;
+            int parsedIntegerInput = 0;
+            Console.WriteLine("Please Enter amount of rows");
+            while (!inputValidation)
+            {
+                string userInput = Console.ReadLine();
+                inputValidation = isUserInputIsValid(userInput, out parsedIntegerInput);
+            }
+            Ex01_02.Program.PrintDiamond(parsedIntegerInput);
+        }
+
+        private static bool isUserInputIsValid(string i_UserInput, out int i_ParsedIntegerInput)
+        {
+            bool isNumber = int.TryParse(i_UserInput, out i_ParsedIntegerInput);
+            if (!isNumber || i_ParsedIntegerInput <= 0)
+            {
+                Console.WriteLine("Invalid input");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
